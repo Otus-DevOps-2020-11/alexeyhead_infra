d5253c6d5f60571615b77cf366bbfcf88a187554
OLeksii Smiichuk <smiychuk@gmail.com>
2021-02-05 22:14:55 +0200

    Hide some access data

--- terraform/stage/backend.tf
+++ terraform/stage/backend.tf
@@ -3,14 +3,12 @@ terraform {
     endpoint = "storage.yandexcloud.net"
     bucket = "odo"
     region = "ru-central1"
-    # key        = "stage/terraform.tfstate"
+    # key        = "prod/terraform.tfstate"
     key        = "terraform.tfstate"
-    access_key = "IiiorWMMHMicvxEm2g2R"
-    secret_key = "UnSvHaUDcIyyypNeZl-2rJl9G-OQLmy5zIvr5F8l"
-    #access_key = "ToMA0qxN5koRN2RIU15d"
-    #secret_key = "1_P3q1bTXBuD7HEgCEjV_1pH9tZLxG-qesqMAiIw"
-    dynamodb_endpoint = "https://docapi.serverless.yandexcloud.net/ru-central1/b1gr7ijt35laouif2ch5/etn03jtomlsin4ke6mo4"
-    dynamodb_table = "terraform_lock_state"
+    access_key = "some_access_key"
+    secret_key = "some_secret_key"
+    dynamodb_endpoint = "https://docapi.serverless.yandexcloud.net/dynamodb_endpoint"
+    dynamodb_table = "some_dynamodb_table"
 
     skip_region_validation     = true
     skip_credentials_validation = true
